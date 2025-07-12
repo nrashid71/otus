642ca644fa731faaf9dd5dcd4919e140b83ccc82
nrashid <nrashid@inbox.ru>
2025-07-12 12:21:05 +0300

    Модель базы данных. Исправления после CR.

--- HomeWorks/Bot/db/ToDoListDb.sql
+++ HomeWorks/Bot/db/ToDoListDb.sql
@@ -1,32 +1,39 @@
 create table if not exists "Infrastructure"."ToDoUser" (
-    "UserId" uuid primary key not null,
+    "UserId" uuid not null,
     "TelegramUserId" bigint not null,
-    "TelegramUserName" varchar(250) null,
-    "RegisteredAt" timestamp not null default now());
-
-create unique index if not exists "IToDoUserTelegramUserId" on "Infrastructure"."ToDoUser"("TelegramUserId");
-
-create table if not exists "Infrastructure"."ToDoList" (
-    "Id" uuid primary key not null,
-    "Name" varchar(250) not null,
-    "ToDoUser" uuid references "ToDoUser"("UserId") not null,
-    "CreatedAt" timestamp not null default now()
+    "TelegramUserName" text null,
+    "RegisteredAt" timestamp not null default now(),
+    constraint "PKToDoUser" primary key ("UserId")
     );
 
-create index if not exists "IToDoListToDoUser" on "Infrastructure"."ToDoList"("ToDoUser");
+create unique index if not exists "UQ_ToDoUser_TelegramUserId" on "Infrastructure"."ToDoUser"("TelegramUserId");
+
+create table if not exists "Infrastructure"."ToDoList" (
+    "Id" uuid not null,
+    "Name" text not null,
+    "ToDoUserId" uuid not null,
+    "CreatedAt" timestamp not null default now(),
+    constraint "PK_ToDoList" primary key ("Id"),
+    constraint "FK_ToDoList_ToDoUserId" foreign key ("ToDoUserId") references "ToDoUser"("UserId")
+    );
+
+create index if not exists "I_ToDoList_ToDoUserId" on "Infrastructure"."ToDoList"("ToDoUserId");
 
 create table if not exists "Infrastructure"."ToDoItem" (
-    "Id" uuid primary key not null,
-    "Name" varchar(250) not null,
-    "ToDoUser" uuid references "ToDoUser"("UserId") not null,
-    "ToDoList" uuid references "ToDoList"("Id") null,
+    "Id" uuid not null,
+    "Name" text not null,
+    "ToDoUserId" uuid not null,
+    "ToDoListId" uuid null,
     "Deadline" timestamp not null,
     "State" integer not null,
     "CreatedAt" timestamp not null default now(),
-    "StateChangedAt" timestamp null
+    "StateChangedAt" timestamp null,
+    constraint "PKToDoItem" primary key ("Id"),
+    constraint "FK_ToDoItem_ToDoUserId" foreign key ("ToDoUserId") references "ToDoUser"("UserId"),
+    constraint "FK_ToDoItem_ToDoListId" foreign key ("ToDoListId") references "ToDoList"("Id")
     );
-create index if not exists "IToDoItemToDoUser" on "Infrastructure"."ToDoItem"("ToDoUser");
-create index if not exists "IToDoItemToDoList" on "Infrastructure"."ToDoItem"("ToDoList");
+create index if not exists "I_ToDoItem_ToDoUserId" on "Infrastructure"."ToDoItem"("ToDoUserId");
+create index if not exists "I_ToDoItem_ToDoListId" on "Infrastructure"."ToDoItem"("ToDoListId");
 
 commit;
 
